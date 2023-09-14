738b190cdb62d227af9595e9759b2991886ef470
Otis Lambert <otislambert@gmail.com>
2023-09-14 13:44:17 -0700

    chore(app): created global.scss
    -moved global styles to global.scss from +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,8 @@
 <script lang="ts">
+  import "../styles/global.scss";
   import HeaderComponent from "../components/HeaderComponent.svelte";
   import Schedule from "../components/Schedule.svelte";
 </script>
 
 <HeaderComponent />
 <Schedule />
-
-<style>
-  :global(html) {
-    font-family: Helvetica, sans-serif;
-    font-size: 15px;
-  }
-</style>
-
