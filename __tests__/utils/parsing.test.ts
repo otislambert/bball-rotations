3d8dcfa55b97cf61ee5459c8034e345691e29ac1
Otis Lambert <otislambert@gmail.com>
2023-09-16 11:56:53 -0700

    feat(parsing): wrote parseURLDate()
    - wrote parseURLDate. this allows for reconstructing date slugs
    - wrote a test suite for parseURLDate

--- __tests__/utils/parsing.test.ts
+++ __tests__/utils/parsing.test.ts
@@ -4,4 +4,19 @@ describe("parseUrlDate", () => {
   it("Throws an error if a string has less than eight characters", () => {
     expect(() => parseURLDate("0234")).toThrowError();
   });
+  it("Throws an error if a string has more than eight characters", () => {
+    expect(() => parseURLDate("02340828291841203")).toThrowError();
+  });
+  it("Throws an error if a string has non-numericals", () => {
+    expect(() => parseURLDate("20a0234a")).toThrowError();
+  });
+  it("Returns a value", () => {
+    expect(parseURLDate("01052024")).toBeDefined();
+  });
+  it("Returns a date", () => {
+    expect(parseURLDate("01052024")).toMatch(/\d\d\/\d\d\/\d\d\d\d/);
+  });
+  it("Returns the exact date and time", () => {
+    expect(parseURLDate("01052024")).toBe("01/05/2024 00:00:00");
+  });
 });
--- src/utils/parsing.ts
+++ src/utils/parsing.ts
@@ -1,7 +1,13 @@
 function parseURLDate(date: string): string {
-  if (date.length != 8) {
+  if (!/^[0-9]{8}$/.test(date)) {
     throw new Error("Unsupported Date Format");
   }
+
+  const m = date.slice(0, 2);
+  const d = date.slice(2, 4);
+  const y = date.slice(4);
+
+  return m + "/" + d + "/" + y + " 00:00:00";
 }
 
 export { parseURLDate };
