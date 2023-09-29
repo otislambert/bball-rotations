b033cb177bf9ee441e1d1fea6a438fc072de5ff4
Otis Lambert <otislambert@gmail.com>
2023-09-29 15:10:23 -0700

    test(options): sort by
    - added sort by select to gamePageSettings.
    - integrated sort select with gamePageOptions.

--- src/components/GamePageSettings.svelte
+++ src/components/GamePageSettings.svelte
@@ -1,10 +1,12 @@
 <!-- Game Page Settings -->
 
 <script lang="ts">
+  import { SortValues } from "$types/enums";
   import { gamePageOptions } from "../stores/gamePageOptions";
 
   $: expanded = $gamePageOptions.expanded;
   $: showDNPs = $gamePageOptions.showInactive;
+  $: sort = $gamePageOptions.sort;
 </script>
 
 <!-- TODO: create tests for game page settings -->
@@ -32,6 +34,14 @@
         gamePageOptions.setShowInactive();
       }}
     />
+  </label>
+  <label data-testid="bs-settings-sort-label">
+    <span>Sort by: </span>
+    <select bind:value={$gamePageOptions.sort}>
+      <option value={SortValues.default}>Default</option>
+      <option value={SortValues.minutes}>Minutes</option>
+      <option value={SortValues.points}>Points</option>
+    </select>
   </label>
 </div>
 
